87cbf9cd6094c73fa6738949bed003123f4c84ac
dharma-byte <puttadharmateja6@gmail.com>
2026-07-31 14:30:12 +0530

    Add AuthService wrapper around FirebaseAuth
    Thin wrapper exposing signUp, signIn, signOut, and authStateChanges so features never touch FirebaseAuth directly.

---
+++ lib/core/services/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  AuthService(this._auth);
+
+  final FirebaseAuth _auth;
+
+  Stream<User?> authStateChanges() => _auth.authStateChanges();
+
+  User? get currentUser => _auth.currentUser;
+
+  Future<UserCredential> signUp({
+    required String email,
+    required String password,
+  }) {
+    return _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signOut() => _auth.signOut();
+}
