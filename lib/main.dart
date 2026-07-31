61e449a0c8a7c37922c21a65312f4c26aa5cad7e
dharma-byte <puttadharmateja6@gmail.com>
2026-07-31 14:30:39 +0530

    Wrap app in ProviderScope
    Enables Riverpod providers app-wide, needed for the upcoming auth state provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
 import 'firebase_options.dart';
@@ -7,5 +8,5 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const WellStrideApp());
+  runApp(const ProviderScope(child: WellStrideApp()));
 }
